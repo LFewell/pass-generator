7c0651ac30f4c796968db45b33ed0178668c9aa1
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-16 16:30:51 -0400

    Started defining generatePassword and added a password length check

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,29 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  console.log("You clicked the button!");
+  var passwordLength = window.prompt("How many characters would you like your password to contain? 8-128 characters");
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please select a length between 8 and 128 characters");
+    return;
+  } else {
+    var chosenCharacters = [];
+    console.log(chosenCharacters);
+    var lowerCase = window.confirm("Would you like to include lowercase letters?");
+    if (lowerCase === true) {
+      chosenCharacters.push(0);
+    }
+
+    var upperCase = window.confirm("Would you like to include uppercase letters?");
+    if (upperCase === true) {
+      chosenCharacters.push(1);
+    }
+
+  
+
+  return "Password will display here"
+}
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
