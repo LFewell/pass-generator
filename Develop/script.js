6d9ad47d2820b8ec5ea9ca0121417b4593fcd65c
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-17 02:05:44 -0400

    Swapped out push for concat, wrote randomization logic, and displayed the password

--- Develop/script.js
+++ Develop/script.js
@@ -11,28 +11,58 @@ function writePassword() {
 }
 
 function generatePassword() {
-  console.log("You clicked the button!");
+
+  lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  numbers = "0123456789";
+  specialChars = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var passArray = "";
+
+  //console.log("You clicked the button!");
   var passwordLength = window.prompt("How many characters would you like your password to contain? 8-128 characters");
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Please select a length between 8 and 128 characters");
     return;
   } else {
-    var chosenCharacters = [];
-    console.log(chosenCharacters);
-    var lowerCase = window.confirm("Would you like to include lowercase letters?");
-    if (lowerCase === true) {
-      chosenCharacters.push(0);
+
+    var lowerAdd = window.confirm("Would you like to include lowercase letters?");
+    if (lowerAdd === true) {
+      passArray = passArray.concat(lowerCase);
+    }
+    
+
+    var upperAdd = window.confirm("Would you like to include uppercase letters?");
+    if (upperAdd === true) {
+      passArray = passArray.concat(upperCase);
     }
 
-    var upperCase = window.confirm("Would you like to include uppercase letters?");
-    if (upperCase === true) {
-      chosenCharacters.push(1);
+    var numberAdd = window.confirm("Would you like to include numbers?");
+    if (numberAdd === true) {
+      passArray = passArray.concat(numbers);
+    }
+
+    var specialAdd = window.confirm("Would you like to include special characters?")
+    if (specialAdd === true) {
+      passArray = passArray.concat(specialChars);
+    }
+
+    userPass = "";
+
+    for (var i = 0; i < passwordLength; ++i) { 
+      var randomPass = passArray[Math.floor(Math.random() * passArray.length)];
+        userPass = userPass.concat(randomPass);
+
+      }
+    
     }
 
   
+return userPass;
+  
+}
 
-  return "Password will display here"
-}
-}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
